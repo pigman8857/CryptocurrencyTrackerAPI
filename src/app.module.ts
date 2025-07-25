84ab7822d15b5a33d35505a9c23eba44d045a2ed
SomkamolV <somkamolv@hotmail.com>
2025-07-25 17:19:18 +0700

    feature/connect-db => Create database.config.ts or DatabaseConfigService that will retrieve all environment from env files which works together with envFilePath in app.module ConfigModule. Also, DatabaseConfigService will be imported as root for typeOrmModule at app.module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { testProviders } from './test.provider';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/database.config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`
-  })],
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService,...testProviders],
+  providers: [AppService],
 })
 export class AppModule {
   
