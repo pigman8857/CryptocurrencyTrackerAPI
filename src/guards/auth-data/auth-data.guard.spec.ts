51679f76a69d23d9404c49025e076ec902d74049
SomkamolV <somkamolv@hotmail.com>
2025-07-27 15:06:45 +0700

    feature/check_login => add AuthData guard to check if email password are presented.

---
+++ src/guards/auth-data/auth-data.guard.spec.ts
@@ -0,0 +1,7 @@
+import { AuthDataGuard } from './auth-data.guard';
+
+describe('AuthDataGuard', () => {
+  it('should be defined', () => {
+    expect(new AuthDataGuard()).toBeDefined();
+  });
+});
