93510738011ca261df4ff9c9ed17acc34f35b4ef
SomkamolV <somkamolv@hotmail.com>
2025-07-28 21:31:42 +0700

    feature/crypto_buy_api => 1.Added unit test for current-user.middleware

--- src/middlewares/current-user/current-user.middleware.spec.ts
+++ src/middlewares/current-user/current-user.middleware.spec.ts
@@ -1,7 +1,53 @@
-// import { CurrentUserMiddleware } from './current-user.middleware';
+import { CurrentUserMiddleware } from './current-user.middleware';
+import { UserService } from 'src/user/user.service';
+import { User } from 'src/user/entities/user.entity';
+const { fn ,mock, spyOn} = jest
+describe('CurrentUserMiddleware', () => {
+  let userService: UserService;
+  let middleware: CurrentUserMiddleware;
 
-// describe('CurrentUserMiddleware', () => {
-//   it('should be defined', () => {
-//     expect(new CurrentUserMiddleware()).toBeDefined();
-//   });
-// });
+  beforeEach(() => {
+    //@ts-ignore
+    userService = {
+      findOne: jest.fn(),
+    };
+    middleware = new CurrentUserMiddleware(userService);
+  });
+
+  it('should be defined', () => {
+    expect(middleware).toBeDefined();
+  });
+
+  it('should do nothing if userID is not in session', async () => {
+    const req: any = { session: {} };
+    const res: any = {};
+    const next = fn();
+
+    await middleware.use(req, res, next);
+
+    expect(req.currentUser).toBeUndefined();
+    expect(userService.findOne).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should assign currentUser if userID is in session', async () => {
+    //@ts-ignore
+    const fakeUser: User = {
+      id: 1,
+      email: 'fakeEmail@somewhere.com',
+      password: 'fakepassword',
+    };
+
+    spyOn(userService,"findOne").mockResolvedValue(fakeUser);
+
+    const req: any = { session: { userID: 1 } };
+    const res: any = {};
+    const next = fn();
+
+    await middleware.use(req, res, next);
+
+    expect(userService.findOne).toHaveBeenCalledWith(1);
+    expect(req.currentUser).toEqual(fakeUser);
+    expect(next).toHaveBeenCalled();
+  });
+});
